34df0673b17304a8ba645ed5222aeefb5879ccb3
Jackson Oliveira <jackson.barros@portoseguro.com.br>
2023-06-12 21:44:22 -0300

    feat: add images and titles

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const TaskOrganizer());
+  runApp(const MyApp());
 }
 
-class TaskOrganizer extends StatelessWidget {
-  const TaskOrganizer({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,18 +14,17 @@ class TaskOrganizer extends StatelessWidget {
       theme: ThemeData(),
       home: Scaffold(
         appBar: AppBar(
+          leading: Container(),
           title: Text("Tarefas"),
         ),
         body: ListView(
           children: [
-            Task("Aprender Flutter"),
-            Task("Aprender RxJava"),
-            Task("Estudar para a prova do Serpro"),
-            Task("Estudar para a prova do Serpro"),
-            Task("Estudar para a prova do Serpro"),
-            Task("Estudar para a prova do Serpro"),
-            Task("Estudar para a prova do Serpro"),
-            Task("Estudar para a prova do Serpro"),
+            Task("Aprender Flutter", 'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+            Task("Aprender RxJava", 'https://cdn-images-1.medium.com/v2/resize:fit:1008/1*HDxfd3bSPwgdQ9A3TthVYA.png'),
+            Task("Prova do Serpro", 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Serpro.svg/1200px-Serpro.svg.png'),
+            Task("Meditar", 'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+            Task("Leitura", 'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg'),
+            Task("Jogar COD", 'https://is3-ssl.mzstatic.com/image/thumb/Purple126/v4/e1/6e/63/e16e6345-266f-ddf1-bda2-4e1f43995b18/AppIcon-1x_U007emarketing-0-9-0-85-220.png/1200x630wa.png'),
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -38,8 +37,9 @@ class TaskOrganizer extends StatelessWidget {
 
 class Task extends StatefulWidget {
   final String titleTask;
+  final String image;
 
-  const Task(this.titleTask, {super.key});
+  const Task(this.titleTask, this.image, {super.key});
 
   @override
   State<Task> createState() => _TaskState();
@@ -71,27 +71,64 @@ class _TaskState extends State<Task> {
                         color: Colors.black26,
                         width: 72,
                         height: 100,
+                        child: Image.network(
+                          widget.image,
+                        fit: BoxFit.contain),
                       ),
                       Container(
-                          width: 200,
-                          child: Text(
-                            widget.titleTask,
-                            style: TextStyle(
-                                fontSize: 24, overflow: TextOverflow.ellipsis),
-                          )),
-                      ElevatedButton(
+                        width: 200,
+                        child: Text(
+                          widget.titleTask,
+                          style: TextStyle(
+                              fontSize: 24, overflow: TextOverflow.ellipsis),
+                        ),
+                      ),
+                      Container(
+                        height: 52,
+                        width: 52,
+                        child: ElevatedButton(
                           onPressed: () {
                             setState(() {
                               level++;
                             });
                           },
-                          child: Icon(Icons.arrow_drop_up))
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.arrow_drop_up,
+                              ),
+                              Text(
+                                'UP',
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
-                Text(
-                  'Nivel: $level',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: level / 10,
+                        ),
+                        width: 200,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Text(
+                        'Nivel: $level',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             )
