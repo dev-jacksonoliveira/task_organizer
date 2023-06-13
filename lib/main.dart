3b82bd972fffbe8cab37abb89ad357dc9bd12370
Jackson Oliveira <jackson.barros@portoseguro.com.br>
2023-06-12 23:28:04 -0300

    feat: adds the animated opacity widget, and the logic to hide tasks when clicking on the button

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacityValue = true;
 
   @override
   Widget build(BuildContext context) {
@@ -17,36 +24,45 @@ class MyApp extends StatelessWidget {
           leading: Container(),
           title: Text("Tarefas"),
         ),
-        body: ListView(
-          children: [
-            Task(
-                "Aprender Flutter",
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                3),
-            Task(
-                "Aprender RxJava",
-                'https://cdn-images-1.medium.com/v2/resize:fit:1008/1*HDxfd3bSPwgdQ9A3TthVYA.png',
-                4),
-            Task(
-                "Prova do Serpro",
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Serpro.svg/1200px-Serpro.svg.png',
-                5),
-            Task(
-                "Meditar",
-                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-                4),
-            Task(
-                "Leitura",
-                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
-                2),
-            Task(
-                "Jogar COD",
-                'https://is3-ssl.mzstatic.com/image/thumb/Purple126/v4/e1/6e/63/e16e6345-266f-ddf1-bda2-4e1f43995b18/AppIcon-1x_U007emarketing-0-9-0-85-220.png/1200x630wa.png',
-                4),
-          ],
+        body: AnimatedOpacity(
+          opacity: opacityValue ? 1 : 0,
+          duration: Duration(milliseconds: 800),
+          child: ListView(
+            children: [
+              Task(
+                  "Aprender Flutter",
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                  3),
+              Task(
+                  "Aprender RxJava",
+                  'https://cdn-images-1.medium.com/v2/resize:fit:1008/1*HDxfd3bSPwgdQ9A3TthVYA.png',
+                  4),
+              Task(
+                  "Prova do Serpro",
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Serpro.svg/1200px-Serpro.svg.png',
+                  5),
+              Task(
+                  "Meditar",
+                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+                  4),
+              Task(
+                  "Leitura",
+                  'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                  2),
+              Task(
+                  "Jogar COD",
+                  'https://is3-ssl.mzstatic.com/image/thumb/Purple126/v4/e1/6e/63/e16e6345-266f-ddf1-bda2-4e1f43995b18/AppIcon-1x_U007emarketing-0-9-0-85-220.png/1200x630wa.png',
+                  4),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              opacityValue = !opacityValue;
+            });
+          },
+          child: Icon(Icons.remove_red_eye),
         ),
       ),
     );
