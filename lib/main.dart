430212224f9be21466b393699facb7274b3657c0
Jackson Oliveira <jackson.barros@portoseguro.com.br>
2023-06-12 22:23:21 -0300

    feat: adds difficulty star and ternary condition logic

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,30 @@ class MyApp extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            Task("Aprender Flutter", 'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
-            Task("Aprender RxJava", 'https://cdn-images-1.medium.com/v2/resize:fit:1008/1*HDxfd3bSPwgdQ9A3TthVYA.png'),
-            Task("Prova do Serpro", 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Serpro.svg/1200px-Serpro.svg.png'),
-            Task("Meditar", 'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
-            Task("Leitura", 'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg'),
-            Task("Jogar COD", 'https://is3-ssl.mzstatic.com/image/thumb/Purple126/v4/e1/6e/63/e16e6345-266f-ddf1-bda2-4e1f43995b18/AppIcon-1x_U007emarketing-0-9-0-85-220.png/1200x630wa.png'),
+            Task(
+                "Aprender Flutter",
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                3),
+            Task(
+                "Aprender RxJava",
+                'https://cdn-images-1.medium.com/v2/resize:fit:1008/1*HDxfd3bSPwgdQ9A3TthVYA.png',
+                4),
+            Task(
+                "Prova do Serpro",
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Serpro.svg/1200px-Serpro.svg.png',
+                5),
+            Task(
+                "Meditar",
+                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+                4),
+            Task(
+                "Leitura",
+                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                2),
+            Task(
+                "Jogar COD",
+                'https://is3-ssl.mzstatic.com/image/thumb/Purple126/v4/e1/6e/63/e16e6345-266f-ddf1-bda2-4e1f43995b18/AppIcon-1x_U007emarketing-0-9-0-85-220.png/1200x630wa.png',
+                4),
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -38,8 +56,9 @@ class MyApp extends StatelessWidget {
 class Task extends StatefulWidget {
   final String titleTask;
   final String image;
+  final int difficulty;
 
-  const Task(this.titleTask, this.image, {super.key});
+  const Task(this.titleTask, this.image, this.difficulty, {super.key});
 
   @override
   State<Task> createState() => _TaskState();
@@ -71,17 +90,62 @@ class _TaskState extends State<Task> {
                         color: Colors.black26,
                         width: 72,
                         height: 100,
-                        child: Image.network(
-                          widget.image,
-                        fit: BoxFit.contain),
+                        child: Image.network(widget.image, fit: BoxFit.cover),
                       ),
-                      Container(
-                        width: 200,
-                        child: Text(
-                          widget.titleTask,
-                          style: TextStyle(
-                              fontSize: 24, overflow: TextOverflow.ellipsis),
-                        ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 200,
+                            child: Text(
+                              widget.titleTask,
+                              style: TextStyle(
+                                fontSize: 24,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.difficulty >= 1)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.difficulty >= 2)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.difficulty >= 3)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.difficulty >= 4)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.difficulty >= 5)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                            ],
+                          )
+                        ],
                       ),
                       Container(
                         height: 52,
@@ -116,7 +180,9 @@ class _TaskState extends State<Task> {
                       child: Container(
                         child: LinearProgressIndicator(
                           color: Colors.white,
-                          value: level / 10,
+                          value: (widget.difficulty > 0)
+                              ? (level / widget.difficulty) / 10
+                              : 1,
                         ),
                         width: 200,
                       ),
